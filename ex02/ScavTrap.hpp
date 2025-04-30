33b35b76a691d941782ebd736c1f577e06e60470
Houdaifa Drahm <hdrahm@c3r10p8.1337.ma>
2025-04-30 01:33:28 +0100

    hdrahm<apr 30> : implement inheritance exercises

---
+++ ex02/ScavTrap.hpp
@@ -0,0 +1,20 @@
+# ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+
+class ScavTrap : public ClapTrap
+{
+    public :
+
+        ScavTrap( void );
+        ScavTrap ( const std::string &name);
+        ~ScavTrap ( void );
+        ScavTrap( const ScavTrap &ref);
+        ScavTrap &operator=( const ScavTrap &ref);
+        void guardGate();
+        void attack(const std::string& target);
+};
+
+# endif
